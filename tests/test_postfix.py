02ceff984a5603c61deb720f808e3253888b1b65
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-11-08 16:39:27 +0100

    Add parenthesis precedence test

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -35,6 +35,20 @@ class TestPostfix(unittest.TestCase):
         result = self.postfix.postfix_eval("1 2 + 3 - 4 + 5 - 6 + 7 - 8 + 9 10 * -")
         self.assertEqual(result, "-84")
 
+    def test_parenthesisPrecedence(self):
+        result = self.postfix.postfix_eval("2 3 + 4 5 + *")
+        self.assertEqual(result, "45")
+        result = self.postfix.postfix_eval("2 3 + 4 *")
+        self.assertEqual(result, "20")
+        result = self.postfix.postfix_eval("2 3 4 * +")
+        self.assertEqual(result, "14")
+        result = self.postfix.postfix_eval("2 3 4 * +")
+        self.assertEqual(result, "14")
+        result = self.postfix.postfix_eval("2 3 + 5 * 10 -")
+        self.assertEqual(result, "15")
+        result = self.postfix.postfix_eval("2 3 + 5 * 0 10 - -")
+        self.assertEqual(result, "35")
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
