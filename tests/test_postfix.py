6b29d4f73be6a008d6bad8b29ad7ec648b7b2afb
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-11-08 16:37:36 +0100

    Add binary operators precedence test

--- tests/test_postfix.py
+++ tests/test_postfix.py
@@ -21,6 +21,20 @@ class TestPostfix(unittest.TestCase):
         result = self.postfix.postfix_eval("2 3 /")
         self.assertEqual(result[0:12], "0.6666666666")
 
+    def test_binaryOperatorsPrecedence(self):
+        result = self.postfix.postfix_eval("2 3 + 5 -")
+        self.assertEqual(result, "0")
+        result = self.postfix.postfix_eval("2 3 5 * -")
+        self.assertEqual(result, "-13")
+        result = self.postfix.postfix_eval("2 3 * 5 /")
+        self.assertEqual(result, "1.2")
+        result = self.postfix.postfix_eval("2 3 / 5 +")
+        self.assertEqual(result[0:12], "5.6666666666")
+        result = self.postfix.postfix_eval("1 2 + 3 - 4 + 5 - 6 + 7 - 8 + 9 -")
+        self.assertEqual(result, "-3")
+        result = self.postfix.postfix_eval("1 2 + 3 - 4 + 5 - 6 + 7 - 8 + 9 10 * -")
+        self.assertEqual(result, "-84")
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
